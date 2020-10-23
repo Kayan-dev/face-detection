2f708ae0a5229da7a56f9d639ee2193780196aab
Kayan-dev <kaya-development@outlook.com>
2020-10-23 10:58:47 +0200

    initial commit face detection app

---
+++ src/components/Navigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <nav style={{ display: "flex", justifyConent: "flex" }}>
+      <p>Sign Out</p>
+    </nav>
+  );
+}
