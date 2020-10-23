04559d993a228531a6af9e1b3c4a28ec27e81e8f
Kayan-dev <kaya-development@outlook.com>
2020-10-23 11:46:41 +0200

    adding nice logo

--- src/components/Recognition/FaceRecognition.js
+++ src/components/Recognition/FaceRecognition.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function FaceRecognition() {
-  return <p>something face</p>;
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <p>something face</p>
+    </div>
+  );
 }
