2cb2beb8ce0a6a4062d03ee43af6438270bb2bab
Kayan-dev <kaya-development@outlook.com>
2020-10-24 19:58:25 +0200

    adding signin comp. & refactoring app to homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
-import Logo from "./components/Logo/Logo";
-import ImageLinkForm from "./components/Form/ImageLinkForm";
-import FaceRecognition from "./components/Recognition/FaceRecognition";
+import React from "react";
 import Navigation from "./components/Navigation/Navigation";
-import Rank from "./components/Rank/Rank";
 import "./App.css";
 import Particles from "react-particles-js";
-import Clarifai, { COLOR_MODEL } from "clarifai";
+import HomePage from "./Pages/HomePage";
 
-const app = new Clarifai.App({
-  apiKey: "b4723ee82b5049b6a08801926886ff93",
-});
 const ParticleOptions = {
   particles: {
     number: {
@@ -23,58 +16,11 @@ const ParticleOptions = {
   },
 };
 function App() {
-  const [image, Set_Image] = useState("");
-  const [input, Set_Input] = useState("");
-  const [box, Set_Box] = useState({});
-
-  const calcFaceLocation = (data) => {
-    console.log("What is data", data);
-    const faceLocation =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
-    const boxImage = document.getElementById("inputImage");
-    const width = Number(boxImage.width);
-    const height = Number(boxImage.height);
-
-    return {
-      leftCol: faceLocation.left_col * width,
-      topRow: faceLocation.top_row * height,
-      rightCol: width - faceLocation.right_col * width,
-      bottomRow: height - faceLocation.bottom_row * height,
-    };
-  };
-  //console.log("FACE", calcFaceLocation());
-  const displayBox = (boxer) => {
-    console.log("boxer", boxer);
-    Set_Box(boxer);
-    console.log("Box", box);
-  };
-
-  const onInputChange = (event) => {
-    Set_Input(event.target.value);
-  };
-
-  const onSubmitChange = (event) => {
-    Set_Image(input);
-
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
-      (response) => displayBox(calcFaceLocation(response))
-      // .catch((err) => {
-      //   console.log(err);
-      // })
-    );
-  };
-
   return (
     <div className="App">
       <Particles className="particles" params={ParticleOptions} />
       <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm
-        onInputChange={onInputChange}
-        onSubmitChange={onSubmitChange}
-      />
-      <FaceRecognition box={box} imageUrl={image} />
+      <HomePage />
     </div>
   );
 }
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Logo from "../components/Logo/Logo";
+import ImageLinkForm from "../components/Form/ImageLinkForm";
+import FaceRecognition from "../components/Recognition/FaceRecognition";
+import SignIn from "../components/SignIn/SignIn";
+import Rank from "../components/Rank/Rank";
+import Clarifai, { COLOR_MODEL } from "clarifai";
+
+const app = new Clarifai.App({
+  apiKey: "b4723ee82b5049b6a08801926886ff93",
+});
+
+export default function HomePage() {
+  const [image, Set_Image] = useState("");
+  const [input, Set_Input] = useState("");
+  const [box, Set_Box] = useState({});
+  const [route, Set_Route] = useState("signedout");
+
+  const calcFaceLocation = (data) => {
+    console.log("What is data", data);
+    const faceLocation =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const boxImage = document.getElementById("inputImage");
+    const width = Number(boxImage.width);
+    const height = Number(boxImage.height);
+
+    return {
+      leftCol: faceLocation.left_col * width,
+      topRow: faceLocation.top_row * height,
+      rightCol: width - faceLocation.right_col * width,
+      bottomRow: height - faceLocation.bottom_row * height,
+    };
+  };
+  //console.log("FACE", calcFaceLocation());
+  const displayBox = (boxer) => {
+    console.log("boxer", boxer);
+    Set_Box(boxer);
+    console.log("Box", box);
+  };
+
+  const onInputChange = (event) => {
+    Set_Input(event.target.value);
+  };
+
+  const onSubmitChange = (event) => {
+    Set_Image(input);
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
+      (response) => displayBox(calcFaceLocation(response))
+      // .catch((err) => {
+      //   console.log(err);
+      // })
+    );
+  };
+
+  return (
+    <div className="Homepage">
+      {route === "signin" ? (
+        <SignIn />
+      ) : (
+        <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onSubmitChange={onSubmitChange}
+          />
+          <FaceRecognition box={box} imageUrl={image} />
+        </div>
+      )}
+    </div>
+  );
+}
