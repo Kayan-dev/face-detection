42406e92420c33fb9bfbf8095c380f212f23cfa2
Kayan-dev <kaya-development@outlook.com>
2020-11-04 21:49:08 +0100

    changing api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl =
-  process.env.API_URL || "https://evening-hamlet-82472.herokuapp.com/";
+  process.env.API_URL || "https://evening-hamlet-82472.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
