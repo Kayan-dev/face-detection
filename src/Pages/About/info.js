bf7a0fcd6c04a6b176390995bdbca19a8ab07e34
Kayan-dev <kaya-development@outlook.com>
2020-11-10 15:33:39 +0100

    cleanup

--- src/Pages/About/info.js
+++ src/Pages/About/info.js
@@ -3,8 +3,7 @@ export const info = [
     id: 1,
     title: "Clarifai",
     logo: "https://avatars1.githubusercontent.com/u/8550654?s=400&v=4",
-    text:
-      "The AI from clarifai has been used to detect faces. Click to visit their page",
+    text: "The AI from clarifai has been used to detect faces",
     link: "https://www.clarifai.com/models/face-detection",
   },
   {
@@ -12,8 +11,7 @@ export const info = [
     title: "Codaisseur",
     logo:
       "https://media-exp1.licdn.com/dms/image/C560BAQFW0NMKfVb4Sg/company-logo_200_200/0?e=2159024400&v=beta&t=cmmUjey-wfqjnyRG0yRUgUlBShfJbduug1GcrF1QH38",
-    text: `The backend template from Codaissuer has been used for this project.
-      Click to visit their repository`,
+    text: `The backend template from Codaissuer has been used`,
     link: "https://codaisseur.com/",
   },
   {
@@ -21,8 +19,7 @@ export const info = [
     title: "Zero to Mastery",
     logo:
       "https://res.cloudinary.com/practicaldev/image/fetch/s--12Jli22Q--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/organization/profile_image/1490/f64f3287-bbac-463b-ac3a-d79175b8c427.png",
-    text:
-      "The idea of this face detection app is derived from ZTM. Click to visit their repository",
+    text: "The idea of this face detection app is derived from ZTM",
     link: "https://zerotomastery.io/",
   },
 ];
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,4 @@
 export const apiUrl =
-  process.env.API_URL ||
-  // "https://evening-hamlet-82472.herokuapp.com";
-  "http://localhost:4001";
+  process.env.API_URL || "https://evening-hamlet-82472.herokuapp.com";
+
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
